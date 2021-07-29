f9ab20bc0e027e21668770f6fadc4554708f2ab2
jaenwawe <nwawe.jae@gmail.com>
2021-07-29 15:00:25 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Your code here
+document.addEventListener("keydown", function(event) {
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+  }); 
+
+
+  document.addEventListener("keydown", function(event) {
+    if (event.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+  }); 
+
+  const dodger = document.getElementById("dodger"); 
+
+
+  function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  } 
+
+
+  
+  function moveDodgerRight() {
+    const leftNumbers= dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    const rightNumbers = leftNumbers - dodger.style.width.replace("px", "") ;
+    const rightMax =  parseInt(rightNumbers, 10);
+
+    if (rightMax < 360 ) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
